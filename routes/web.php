7be5cd217dae74e27936390490dd5707c7e59f8d
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2022-09-12 10:17:36 +0700

    Slicing tema sb-admin2

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,5 @@ Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.e
 Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
 
 Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+Route::get('/admin', fn () => view('admin'));
